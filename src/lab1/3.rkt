7a37e4f103d29da5d41a3058842e0ca2f0c24553
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-09-14 15:54:01 +0300

    Сделал задание 1.3

--- src/lab1/3.rkt
+++ src/lab1/3.rkt
@@ -1,17 +1,27 @@
 #lang slideshow
 
-(define bar-width 5)
+(define bar-width 2)
 (define bar-height 100)
+(define separator-height 115)
 
-(define (bar-lines digits)
+(define (bar-line digit height)
+  (cond
+    [(= digit 1)
+     (colorize (filled-rectangle bar-width height) "black")]
+    [else
+     (colorize (filled-rectangle bar-width height) "white")]))
+
+(define (bar-lines-with-height digits height)
   (apply hc-append
          (map (lambda (digit)
-                (cond
-                  [(= digit 1)
-                   (colorize (filled-rectangle bar-width bar-height) "black")]
-                  [else
-                   (colorize (filled-rectangle bar-width bar-height) "white")]))
+                (bar-line digit height))
               digits)))
+
+(define (bar-lines digits)
+  (bar-lines-with-height digits bar-height))
+
+(define (bar-separator)
+  (bar-lines-with-height '(0 1 0 1 0) separator-height))
 
 (define (l-code digit)
   (case digit
@@ -50,4 +60,38 @@
     structure
     digits)))
 
-(bar-encode-with '(L G R) '(1 2 3))
+(define (bar-code-EAN-8 digits)
+  (ht-append
+   (bar-separator)
+   (bar-lines (bar-encode-with '(L L L L) (take digits 4)))
+   (bar-separator)
+   (bar-lines (bar-encode-with '(R R R R) (drop digits 4)))
+   (bar-separator)))
+
+(define (ean13-structure first-digit)
+  (case first-digit
+    [(0) '(L L L L L L)]
+    [(1) '(L L G L G G)]
+    [(2) '(L L G G L G)]
+    [(3) '(L L G G G L)]
+    [(4) '(L G L L G G)]
+    [(5) '(L G G L L G)]
+    [(6) '(L G G G L L)]
+    [(7) '(L G L G L G)]
+    [(8) '(L G L G G L)]
+    [(9) '(L G G L G L)]
+    [else (error "Invalid first digit for EAN-13")]))
+
+(define (bar-code-EAN-13 digits)
+  (ht-append
+   (bar-separator)
+   (bar-lines (bar-encode-with
+               (ean13-structure (first digits))
+               (take (rest digits) 6)))
+   (bar-separator)
+   (bar-lines (bar-encode-with
+               '(R R R R R R)
+               (drop digits 7)))
+   (bar-separator)))
+
+(bar-code-EAN-13 '(5 9 0 1 2 3 4 1 2 3 4 5 7))
