1386b931dd67177fe79bd5012a7b988d72e94ca1
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2026-01-20 11:19:08 +0300

    доделал ракет

--- src/exam/racket.rkt
+++ src/exam/racket.rkt
@@ -1,33 +1,34 @@
 #lang slideshow
 
-;; Вспомогательная функция для безопасного получения n элементов
-;; Возвращает два значения: список элементов (chunk) и остаток потока (rest)
 (define (take-n stream count acc)
-  (if (or (zero? count) (stream-empty? stream))
-      (values (reverse acc) stream)
-      (take-n (stream-rest stream)
-              (sub1 count)
-              (cons (stream-first stream) acc))))
+  (cond
+    [(or (= count 0) (stream-empty? stream))
+     (values acc stream)]
+    [else
+     (take-n (stream-rest stream)
+             (- count 1)
+             (append acc (list (stream-first stream))))]))
 
 (define (stream-chunks st n)
-  (if (stream-empty? st)
-      empty-stream
-      (let-values ([(chunk rest) (take-n st n '())])
-        (stream-cons chunk (stream-chunks rest n)))))
+  (cond
+    [(stream-empty? st) empty-stream]
+    [else
+     (let-values ([(chunk rest) (take-n st n '())])
+       (stream-cons chunk (stream-chunks rest n)))]))
 
 (for/list ([i 3] [chunk (stream-chunks (in-naturals 1) 4)]) chunk)
-; '((1 2 3 4) (5 6 7 8) (9 10 11 12))
+
 
 (define (stream-splits lst)
   (define (helper prefix suffix)
     (stream-cons
-     (cons prefix suffix)   
-     (if (null? suffix)
-         empty-stream           ; Условие выхода
-         (helper (append prefix (list (car suffix))) ; Рекурсивный вызов
-                 (cdr suffix)))))
+     (cons prefix suffix)
+     (cond
+       [(null? suffix) empty-stream]
+       [else
+        (helper (append prefix (list (first suffix)))
+                (rest suffix))])))
   (helper '() lst))
 
+
 (stream->list (stream-splits '(a b c)))
-
-
